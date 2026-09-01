fbc39f26a7b44ea2cc376b812343baa5317ec559
Neelanjan Sarkar <neelanjansarkar78@gmail.com>
2026-09-02 00:32:25 +0530

    Time: 2 ms (99.34%) | Memory: 47.3 MB (11.26%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,22 +1,16 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // Create a hash map to store numbers and their corresponding indices
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        // Iterate through the array
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            // If the complement is already in the map, we found the pair
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+    public int[] twoSum(int[] arr, int target) {
+        HashMap <Integer,Integer> map=new HashMap<>();
+        int[] ans=new int[2];
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(target-arr[i])){
+                ans[0]=i;
+                ans[1]=map.get(target-arr[i]);
+                break;
+            }else{
+                map.put(arr[i],i);
             }
-            
-            // Otherwise, store the current number and its index in the map
-            map.put(nums[i], i);
         }
-        
-        // Return an empty array if no solution is found (per problem constraints, this won't happen)
-        return new int[] {};
+        return ans;
     }
 }
